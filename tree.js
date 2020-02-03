7d9feee5705b80d4f10eb8e545dc8f1a02c10aef
Joseph Richardson <8richardsonj@gmail.com>
2020-02-03 01:59:58 +0000

    Initial Project set up and shape creation

---
+++ tree.js
@@ -0,0 +1,29 @@
+"use strict";
+var bigTree = {
+    numLeaves: '100',
+    averageLeafSize: '40',
+    maxRadius: '250',
+    baseColor: '#aaaaaa',
+    maxLeafHeight: '5',
+    minLeafHeight: '3'
+};
+var smallTree = {
+    numLeaves: '30',
+    averageLeafSize: '30',
+    maxRadius: '100',
+    baseColor: '#aaaaaa',
+    maxLeafHeight: '3',
+    minLeafHeight: '2'
+};
+var bush = {
+    numLeaves: '30',
+    averageLeafSize: '10',
+    maxRadius: '50',
+    baseColor: '#aaaaaa',
+    maxLeafHeight: '1',
+    minLeafHeight: '0'
+};
+var test = {
+    a: '10',
+    b: '20'
+};
